7780d57233de2bbdddf4ca5158dc8d4c4491ae68
Serhii Pirho <pirgosp86@gmail.com>
2025-03-26 18:27:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function placeOrder() {
     document.querySelectorAll('input[type=checkbox]:checked').forEach((el) => {
         toppings.push(el.value);
     });
-    let summary = `Ste naročili ${size} pizzo z ${toppings.length ? toppings.join(", ") : "brez sestavin"}.`;
-    document.getElementById("Vaše naročilo").innerText = summary;
+    let summary = `Ste narocili ${size} pizzo s ${toppings.length ? toppings.join(", ") : "brez sestavin"}.`;
+    document.getElementById("orderSummary").innerText = summary;
 }
